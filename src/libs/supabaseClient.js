c58bb7341a29dc1710216a26604c4a10fe66f9e4
BarameeEiEi <s6519410017@sau.ac.th>
2025-11-20 12:47:20 +0700

    V.5

--- src/libs/supabaseClient.js
+++ src/libs/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from "npm:@supabase/supabase-js@2";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_PROJECT_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_API_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient('https://amqnzuluqtvvpaylslbm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtcW56dWx1cXR2dnBheWxzbGJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwMDU5MjAsImV4cCI6MjA3ODU4MTkyMH0.k1raDLkBDs3hhfLofpkBWGWCVKFykRUyhj2jJTVCyyw');
